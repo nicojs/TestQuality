632c7aef955a8f53429580a63c6d979899fb21ef
c0ncentus <jeremymartin33610@gmail.com>
2019-07-04 08:03:44 +0200

    travis test

--- stryker.conf.js
+++ stryker.conf.js
@@ -2,7 +2,7 @@ module.exports = function(config) {
   config.set({
     mutator: "typescript",
     packageManager: "npm",
-    reporters: ["html", "clear-text","dashboard"],
+    reporters: ["html","dashboard"],
     testRunner: "mocha",
     transpilers: ["typescript"],
     testFramework: "mocha",
