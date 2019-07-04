8d4043e22760a683ae23d05ba45e1e7a65b93a8e
Nico Jansen <nico.jansen@infosupport.com>
2019-07-04 22:13:17 +0200

    Configure stryker with typescript and mocha

--- stryker.conf.js
+++ stryker.conf.js
@@ -1,13 +1,21 @@
 module.exports = function(config) {
   config.set({
-    mutator: "typescript",
-    packageManager: "npm",
-    reporters: ["html","dashboard"],
-    testRunner: "mocha",
-    transpilers: [],
-    testFramework: "mocha",
-    coverageAnalysis: "off",
-    tsconfigFile: "tsconfig.json",
-    mutate: ["src/**"]
+    mutator: 'typescript',
+    packageManager: 'npm',
+    reporters: ['html','dashboard'],
+    testRunner: 'mocha',
+    transpilers: [
+      'typescript'
+    ],
+    testFramework: 'mocha',
+    coverageAnalysis: 'perTest',
+    tsconfigFile: 'tsconfig.json',
+    mutate: ['src/**'],
+    maxConcurrentTestRunners: 1,
+    mochaOptions: {
+      spec: [
+        'Test/**/*.js'
+      ]
+    }
   });
 };
